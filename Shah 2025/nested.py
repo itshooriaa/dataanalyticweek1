14c11340031fdef3e9facd7b98541d46caa6ffb2
itshooriaa <siddiiqui12@gmail.com>
2025-03-19 16:53:14 +0500

    new one

---
+++ Shah 2025/nested.py
@@ -0,0 +1,13 @@
+var1=input("enter your product name:")
+price=float(input("enter your budget:"))
+if(price>=90000):
+    if(ram==4):
+        if(size==7):
+            if("color==red"):
+            print("oppo")
+elif(price>=50000):
+    elif(ram==3):
+        elif(size==6):
+            elif("color==black")
+else:
+    print("no nothing")
